805340afa55927d6189d1c332b93fd0cb1a7e2ad
sepiariver <yj@paywith.com>
2022-02-21 12:14:22 -0800

    better example

--- test/examples.spec.js
+++ test/examples.spec.js
@@ -1,28 +1,83 @@
 import Binliner from "../dist";
 
 describe("Binliner", () => {
-  it('decision tree', () => {
-    // first is required (1)
-    // second is optional (10, 11)
-    // third is dependent on second (100, 111)
-    // fourth changes behaviour (1000: 8, 1110: 14, 1001: 9, 1111: 15)
-    let first = () => 1;
-    let second = () => 0;
-    let third = () => 0;
-    let fourth = () => 1;
-    let run = (action) => console.log(String(action));
+  it('handles verbose control flows transparently', () => {
+    const doThing = (thing) => console.log(String(thing));
+    const verboseFlow = (first, second, third, fourth) => {
+      let valid;
+      if (!first) {
+        valid = false; // first is required
+        return valid;
+      }
+      if (!second && third) {
+        valid = false; // third depends on second
+        return valid;
+      }
+      if (second && !third) {
+        valid = false; // third depends on second
+        return valid;
+      }
+      if (second && third) {
+        valid = true;
+        doThing('second and third are both true, continue');
+      }
+      if (!second && !third) {
+        valid = true;
+        doThing('second and third are both false, continue');
+      }
+      if (fourth) {
+        doThing('fourth is true, return foo = bar');
+        return {
+          valid,
+          foo: 'bar'
+        };
+      } else {
+        doThing('fourth is false, return foo = baz');
+        return {
+          valid,
+          foo: 'baz'
+        };
+      }
+    };
+    const binLinerFlow = (first, second, third, fourth) => {
+      let valid;
+      const bin = new Binliner({
+        size: 4,
+        validation: [8, 9, 14, 15]
+      }, first, second, third, fourth); // Represent conditions as binary stream: 1000, 1001, etc.
+      if (!bin.isValid()) {
+        valid = false; // capture all invalid cases
+        return valid;
+      }
+      if (Number(bin) > 10) { // 1110: 14, 1111: 15
+        doThing('second and third are both true, continue');
+      } else { // 1000: 8, 1001: 9
+        doThing('second and third are both false, continue');
+      }
+      if (bin.get(3) === 0) { // 1000: 8, 1110: 14
+        doThing('fourth is false, return foo = baz');
+        return {
+          valid,
+          foo: 'baz'
+        };
+      } else { // 1001: 9, 1111: 15
+        doThing('fourth is true, return foo = bar');
+        return {
+          valid,
+          foo: 'bar'
+        };
+      }
+    }
+    expect(verboseFlow(false, true, true, true)).toBeFalsy();
+    expect(verboseFlow(true, true, false, true)).toBeFalsy();
+    expect(verboseFlow(true, false, true, true)).toBeFalsy();
+    expect(verboseFlow(true, true, true, true)).toBeTruthy();
+    expect(verboseFlow(true, false, false, false)).toBeTruthy();
 
-    let valid = true;
-    if (!first()) valid = false;
-    if ((second()) !== (third())) valid = false;
-    if (valid && fourth()) run('line 18');
-    if (valid && !fourth()) run('line 19');
-    run('line 20');
-
-    let values = [first(), second(), third(), fourth()];
-    let bin = new Binliner({size: 4, validation: [8, 9, 14, 15]}, ...values);
-    let results = ['this', 'that'];
-    if (bin.isValid()) run(results[bin.get(3)]);
-    run('line 30');
+    expect(binLinerFlow(false, true, true, true)).toBeFalsy();
+    expect(binLinerFlow(true, true, false, true)).toBeFalsy();
+    expect(binLinerFlow(true, false, true, true)).toBeFalsy();
+    expect(binLinerFlow(true, true, true, true)).toBeTruthy();
+    expect(binLinerFlow(true, false, false, false)).toBeTruthy();
   });
 });
